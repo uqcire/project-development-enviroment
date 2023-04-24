b2df2f3cc74b50375667da584c49a01b353caeb9
Eric <ci_re@foxmail.com>
2023-04-24 13:57:17 +1000

    build(deps): fixed commitizen configs

--- .cz-config.js
+++ .cz-config.js
@@ -1,20 +1,19 @@
 module.exports = {
   types: [
-      {value: 'feature',  name: 'feature:  新增功能'},
-      {value: 'bug',      name: 'bug:      测试反馈bug列表中的bug号'},
-      {value: 'fix',      name: 'fix:      修复bug'},
-      {value: 'ui',       name: 'ui:       更新UI'},
-      {value: 'docs',     name: 'docs:     文档变更'},
-      {value: 'style',    name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）'},
-      {value: 'perf',     name: 'perf:     性能优化'},
-      {value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'},
-      {value: 'release',  name: 'release:  发布'},
-      {value: 'deploy',   name: 'deploy:   部署'},
-      {value: 'test',     name: 'test:     添加、修改测试用例'},
-      {value: 'chore',    name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'},
-      {value: 'revert',   name: 'revert:   回滚 commit'},
-      {value: 'build',    name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 脚手架 配置等）'},
-      {value: 'wip',      name:'wip:       开发中' }
+    { value: 'feat',      name:'feat:      新增功能' },
+    { value: 'fix',       name:'fix:       修复bug' },
+    { value: 'docs',      name:'docs:      文档变更' },
+    { value: 'style',     name:'style:     代码格式（不影响功能，例如空格、分号等格式修正）' },
+    { value: 'refactor',  name:'refactor:  代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'perf',      name:'perf:      性能优化' },
+    { value: 'test',      name:'test:      添加、修改测试用例' },
+    { value: 'build',     name:'build:     构建流程、外部依赖变更（如升级 npm 包、修改 脚手架 配置等）' },
+    { value: 'ci',        name:'ci:        修改 CI 配置、脚本' },
+    { value: 'chore',     name:'chore:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
+    { value: 'revert',    name:'revert:    回滚 commit' },
+    { value: 'wip',       name:'wip:       开发中' },
+    { value: 'mod',       name:'mod:       不确定分类的修改' },
+    { value: 'release',   name:'release:   发布' },
   ],
   scopes: [
     ['custom', '自定义'],
@@ -41,8 +40,6 @@ module.exports = {
     footer: '列举出所有变更的 Issues Closed（可选）。 例如: #31, #34：',
     confirmCommit: '确认提交？'
   },
-  // allowCustomScopes: true,
-  // skipQuestions: ['body', 'footer'],
   allowBreakingChanges: ['feat', 'fix'],
   subjectLimit: 100,
   breaklineChar: '|'
