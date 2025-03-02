3454051ab9f1a7e84f11a17284cd1df9d3483c65
Chen Qu <ci_re@foxmail.com>
2025-03-02 12:26:44 +1100

    ci: update dependencies to their latest compatible versions

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -1,22 +1,25 @@
-import axios from 'axios'
-import { reqReject, reqResolve, resReject, resResolve } from './interceptors'
+import axios from 'axios';
+import qs from 'qs';
+import addReqInterceptor from './reqInterceptor';
+import addResInterceptor from './resInterceptor';
 
-export function createAxios(options = {}) {
-  const defaultOptions = {
-    baseURL: import.meta.env.VITE_APP_BASE_API,
-    timeout: 12000,
-  }
-  const service = axios.create({
-    ...defaultOptions,
-    ...options,
-  })
-  service.interceptors.request.use(reqResolve, reqReject)
-  service.interceptors.response.use(resResolve, resReject)
-  return service
-}
 
-export const defAxios = createAxios()
+// 创建 axios 实例
+const instance = axios.create({
+  baseURL: process.env.$baseUrl, // 在环境变量中设置基础的请求 URL
+  timeout: 1000 * 60,  // 设置请求超时时间，单位是毫秒
+  withCredentials: true, // 跨域请求时是否需要携带 cookie
+  paramsSerializer: function (params) {
+    // 使用 qs 库对请求参数进行序列化，支持数组格式化
+    return qs.stringify(params, { arrayFormat: 'indices' });
+  },
+});
 
-export const testAxios = createAxios({
-  baseURL: import.meta.env.VITE_APP_BASE_API_TEST,
-})
+// 添加请求拦截器
+addReqInterceptor(instance);
+
+// 添加响应拦截器
+addResInterceptor(instance);
+
+export default instance;
+
